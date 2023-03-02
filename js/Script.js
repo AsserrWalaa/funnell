f94be001a031d75b0361f0e6fd3e7c13f7f8a092
AsserrWalaa <111322681+AsserrWalaa@users.noreply.github.com>
2023-03-02 14:16:55 +0200

    Add files via upload

---
+++ js/Script.js
@@ -0,0 +1,16 @@
+// for (let mems of doctors) {
+//         document.write(`
+//                             <div class="card p-3 text-center">
+//                                 <img src="/images/${mems.image}">
+//                                 <h3> name ${mems.Name}</h1>
+//                                     <h4> Age ${mems.Age} </h1>
+//                                         <h5> job ${mems.job} </h1>
+//                                             </div>
+//                                             `)
+//     }
+let doctors = [
+    { id: 1, Name: "Ibrahim", Specialty: "Eyes", Date: "Sunday", Image: "team-1.jpg" } ,
+    { id: 1, Name: "Ahmed", Specialty: "Dental", Date: "Monday" , Image: "team-3.jpg" },
+    { id: 1, Name: "Alaa", Specialty: "Skins", Date: "Friday" , Image: "team-2.jpg"},
+    { id: 1, Name: "Mona", Specialty: "Orthopedist", Date: "Sunday" , Image: "team-4.jpg"}
+] 
